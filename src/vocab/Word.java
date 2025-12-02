66b40e5deb95d5957fbe4e5a1b8d8f50f05449e4
Jangjeongwon <poun030803@naver.com>
2025-12-02 12:24:24 +0900

    word 클래스 wordBook 클래스 2주차 구현

--- src/vocab/Word.java
+++ src/vocab/Word.java
@@ -1,15 +1,18 @@
 package src.vocab;
 
-// 단어 1개 정보
+// 단어 한 개 저장용 클래스
 public class Word {
-    private String english;
-    private String meaning;
 
+    private String english;   // 영어
+    private String meaning;   // 뜻
+
+    // 생성자 
     public Word(String english, String meaning) {
         this.english = english;
         this.meaning = meaning;
     }
 
+    // getter들 (getEnglish , getMeaning)
     public String getEnglish() {
         return english;
     }
@@ -18,8 +21,12 @@ public class Word {
         return meaning;
     }
 
+    
     @Override
     public String toString() {
         return english + " : " + meaning;
     }
+    // 출력할께 알아보기 쉽게하기 위해 선언
+
+    
 }
--- src/vocab/WordBook.java
+++ src/vocab/WordBook.java
@@ -3,15 +3,68 @@ package src.vocab;
 import java.util.ArrayList;
 import java.util.List;
 
-// 여러 단어를 관리
+//단어장 클래스
 public class WordBook {
+
+    // 단어 넣어두는 리스트
     private List<Word> words = new ArrayList<>();
 
+    // 단어 추가 -> 아직은 같은 영어 단어가 있어도 그냥 또 들어는 상태임. 추후 보강예정
     public void add(Word word) {
-        words.add(word);
+        if (word != null) {
+            words.add(word);
+        }
     }
 
+    // 단어장 안에 모든 단어들 돌려주는 기능 (전체 목록 반환)
     public List<Word> getAll() {
         return words;
     }
+
+    // 특정 영어단어 찾는 기능 
+    public Word findByEnglish(String english) {
+        if (english == null) {
+            return null;
+        }
+            
+        for (Word w : words) {
+            if (w.getEnglish().equalsIgnoreCase(english)) {
+                return w;
+            }
+        }
+        return null;
+    }
+
+    // 영어단어로 삭제하는 기능 
+    public boolean removeByEnglish(String english) {
+        Word target = findByEnglish(english);
+        if (target == null) { // 단어가 없을 때 
+            return false;
+        }
+        return words.remove(target); // true상태 -> 삭제
+    }
+
+    // 검색 기능
+    public List<Word> search(String keyword) {
+        List<Word> result = new ArrayList<>(); //result 리스트 추가 
+
+        if (keyword == null || keyword.trim().equals("")) {
+            return result;
+        }
+
+        String k = keyword.toLowerCase();
+
+        for (Word w : words) {
+            String e = w.getEnglish().toLowerCase(); // 대소문자 상관 없이 비교
+                                                    //  -> 단어를 소문자로 바꾸는 원리.
+            String m = w.getMeaning().toLowerCase();
+
+            if (e.contains(k) || m.contains(k)) { // 단어 or 뜻 하나라고 있으면 단어 출력 함. 
+                result.add(w); //result에 추가함. 
+            }
+        }
+
+        return result;
+    }
+
 }
