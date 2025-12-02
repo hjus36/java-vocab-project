b5f94c7146f67ba8e185ce5904b123b5aae6f6e3
이강민 <hjus36@a.ut.ac.kr>
2025-12-02 20:30:47 +0900

    2주차
    수정

--- src/vocab/App.java
+++ src/vocab/App.java
@@ -7,7 +7,7 @@ public class App {
 
         WordBook book = new WordBook(); 
         FileManager fm = new FileManager(); 
-        fm.load(book); // 시작 시 저장된 단어 불러오기 (없으면 안내 메시지)
-        new MainFrame(book, fm); // 메인 GUI 프레임 생성 및 표시
+        fm.load(book); // 시작 시 저장된 단어 불러오기
+        new MainFrame(book, fm); // 메인 GUI 프레임 실행
     }
 }
