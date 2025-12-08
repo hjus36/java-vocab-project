1170961070c5d8b17bdced7cb5f41f8cb2b24805
이강민 <hjus36@a.ut.ac.kr>
2025-12-08 19:46:57 +0900

    3주차
    수정

--- src/vocab/App.java
+++ src/vocab/App.java
@@ -1,21 +1,13 @@
 package src.vocab;
 
-import javax.swing.SwingUtilities;
-
 public class App {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                WordBook book = new WordBook();
-                FileManager fm = new FileManager();
+        WordBook book = new WordBook();
+        FileManager fm = new FileManager();
 
-                // 기본(전공) 단어장 파일이 있으면 불러오기 시도
-                fm.load(book, BookType.MAJOR);
+        fm.load(book, BookType.MAJOR);
 
-                new MainFrame(book, fm);
-            }
-        });
+        new MainFrame(book, fm); 
     }
 }
